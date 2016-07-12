dd21509826a707931413274ac0528a5d66012f63
aungureanu614 <aungureanu614@gmail.com>
2016-07-12 01:13:35 -0700

    js/app.js

--- js/app.js
+++ js/app.js
@@ -4,12 +4,10 @@ $(document).ready(function(){
     var userItem = $('#textbox').val();
     $('ul').append($('<li><span class="check">&#9989;</span>' + userItem + '<span class="delete">&#10008;</span><span class="edit">&#10000;</span></li>'));
       
-  });
+  }),
 
-  $('.delete').click(function(){
-    var userItem = $('#textbox').val();
-    $('ul').remove($('<li><span class="check">&#9989;</span>' + userItem + '<span class="delete">&#10008;</span><span class="edit">&#10000;</span></li>'));
-      
-  });
+ $('ul').on('click', 'li', function(event){
+       $(this).remove();
+});
 
 });
