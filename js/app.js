0a4d02b248857403ac8b57a25d4dbe64aae882f8
aungureanu614 <aungureanu614@gmail.com>
2016-07-13 18:25:38 -0700

    index.html

--- js/app.js
+++ js/app.js
@@ -1,13 +1,26 @@
+
+
 $(document).ready(function(){
      
   $('.btn-add').click(function(){
     var userItem = $('#textbox').val();
-    $('ul').append($('<li><span class="check">&#9989;</span>' + userItem + '<span class="delete">&#10008;</span><span class="edit">&#10000;</span></li>'));
+    $('ul').append($('<li><span class="check">&#9989;</span>' + '<span class="list-item">' + userItem + '</span>' + '<span class="delete">&#10008;</span><span class="edit">&#10000;</span></li>'));
       
   }),
 
- $('ul').on('click', 'li', function(event){
-       $(this).remove();
+ $('ul').on('click', '.delete', function(event){
+       $(this).parent().remove();
+}),
+
+  $('ul').on('click', '.check', function(event){
+       $(this).parent().addClass('cross-out');
+
+}),
+
+   $('ul').on('click', '.edit', function(event){
+       var editItem = $(this).parent().find('.list-item').text();
+       $('#textbox').val(editItem);
+       $(this).parent().remove();
 });
 
 });
